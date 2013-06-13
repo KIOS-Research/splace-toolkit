b9a0a9066768a1c9fcef9b888502a4fdc7064a5d
Demetrios Eliades <eldemet@ucy.ac.cy>
2013-06-13 13:01:33 +0300

    Updates in Licenses

--- SPLACE/MISC/cartesianProduct.m
+++ SPLACE/MISC/cartesianProduct.m
@@ -1,3 +1,9 @@
+%{
+  Copyright: Amro, 2010 (http://stackoverflow.com/users/97160/amro)
+  Stack Overflow, Creative Commons Attribution-ShareAlike 3.0 Unported License
+  http://stackoverflow.com/questions/4165859  
+%}
+
 function result = cartesianProduct(sets)
     c = cell(1, numel(sets));
     [c{:}] = ndgrid( sets{:} );
